31a668fc0a00baed675e6323ac4673086fab073a
dingyang@illiadtechnologies.com <dingyang@illiadtechnologies.com>
2013-07-02 15:10:45 +0800

    add MyOperation.h

---
+++ MyOperation.h
@@ -0,0 +1,15 @@
+//
+//  MyOperation.h
+//  text
+//
+//  Created by Ding Yang on 22/4/13.
+//  Copyright (c) 2013 Ding Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyOperation : NSOperation{
+    NSString *name;
+}
+@property (nonatomic,retain)NSString *name;
+@end
